2d608499fa3ce72a5ca723fa67001801a325abfc
plusakacper <plusakacper@gmail.com>
2023-06-21 20:58:59 +0200

    Add exception handler for fields validation

--- src/main/java/pl/bookapp/rest/error/ExceptionAdvice.java
+++ src/main/java/pl/bookapp/rest/error/ExceptionAdvice.java
@@ -1,6 +1,12 @@
 package pl.bookapp.rest.error;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,6 +21,18 @@ public class ExceptionAdvice {
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public String exceptionHandler(Exception ex) {
 		return ex.getMessage();
+	}
+
+	@ExceptionHandler(value = { MethodArgumentNotValidException.class })
+	@ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
+	public ResponseEntity<Map<String, String>> validationHandler(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		return ResponseEntity.badRequest().body(errors);
 	}
 
 }
